152d5c9c6ea89befc5687ad5a83144bf01d13344
serwk <serwk@rambler.ru>
2019-01-06 07:13:25 +0300

    Изучаем списки

--- snake/snake/snake/Program.cs
+++ snake/snake/snake/Program.cs
@@ -13,8 +13,23 @@ namespace snake
             Point p1 = new Point(1,3,'*');
             p1.Draw();
 
-            Point p2 = new Point(4,5,'#');
+            Point p2 = new Point(4, 5, '#');
             p2.Draw();
+
+            Point p3 = new Point(2, 4, '+');
+            Point p4 = new Point(5, 1, '/');
+
+            List<Point> pList = new List<Point>();
+            pList.Add(p1);
+            pList.Add(p2);
+            pList.Add(p3);
+            pList.Add(p4);
+
+            List<char> cList = new List<char>();
+            cList.Add('*');
+            cList.Add('|');
+            cList.Add('+');
+            cList.Add('=');
 
             Console.ReadLine();
         }
