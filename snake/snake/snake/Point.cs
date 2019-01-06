8394eb03849afc52997073e2ab5a557fb2203c82
serwk <serwk@rambler.ru>
2019-01-06 18:16:59 +0300

    Змейка в движении

--- snake/snake/snake/Point.cs
+++ snake/snake/snake/Point.cs
@@ -19,10 +19,44 @@ namespace snake
             sym = _sym;
         }
 
+        public Point(Point p)
+        {
+            x = p.x;
+            y = p.y;
+            sym = p.sym;
+        }
+
+        public void Move(int offset, Direction direction)
+        {
+            if(direction == Direction.RIGHT)
+            {
+                x = x + offset;
+            }
+            else if (direction == Direction.LEFT)
+            {
+                x = x - offset;
+            }
+            else if (direction == Direction.UP)
+            {
+                y = y + offset;
+            }
+            else
+            {
+                y = y - offset;
+            }
+
+        }
+
         public void Draw()
         {
             Console.SetCursorPosition(x, y);
             Console.Write(sym);
+        }
+
+        public void Clear()
+        {
+            sym = ' ';
+            Draw();
         }
     }
 }
